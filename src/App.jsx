17e2d125fe1e0adf16bde5dd16ee9849b3671529
Jaydeep Suthar <technicaljade@gmail.com>
2022-10-01 18:55:00 +0530

    Portfolio Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -157,7 +157,59 @@ const App = () => {
 			</section>
 
 			<section className="section-1">
-				<div className="main-container">Portfolio</div>
+				<div className="main-container">
+					<h3 style={{ textAlign: "center" }}>Some of My Projects</h3>
+
+					<div className="post-wrapper">
+						<div>
+							<div className="post">
+								<img className="thumbnail" src={image} />
+								<div className="post-preview">
+									<h6 className="post-title">Post Title</h6>
+									<p className="post-intro">
+										this is a demo sentence
+									</p>
+
+									<a target="_blank" href="#">
+										Read More
+									</a>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<div className="post">
+								<img className="thumbnail" src={image} />
+								<div className="post-preview">
+									<h6 className="post-title">Post Title</h6>
+									<p className="post-intro">
+										this is a demo sentence
+									</p>
+
+									<a target="_blank" href="#">
+										Read More
+									</a>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<div className="post">
+								<img className="thumbnail" src={image} />
+								<div className="post-preview">
+									<h6 className="post-title">Post Title</h6>
+									<p className="post-intro">
+										this is a demo sentence
+									</p>
+
+									<a target="_blank" href="#">
+										Read More
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</section>
 
 			<section className="section-1">
